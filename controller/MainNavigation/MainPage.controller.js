a5eb33cf80fdf501a3a8e5c1d1ba72824010d3f8
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2021-08-26 12:04:20 +0200

    Init

--- controller/MainNavigation/MainPage.controller.js
+++ controller/MainNavigation/MainPage.controller.js
@@ -1,9 +1,23 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], function (Controller) {
+    'sap/m/GroupHeaderListItem',
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel'
+], function (GroupHeaderListItem, Controller, JSONModel) {
     "use strict";
 
-    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.MainPage");
+    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.MainPage", {
+        onInit: function (evt) {
+            var oModel = new JSONModel(sap.ui.require.toUrl("DemoUXapp/localService/mockdata/products.json"));
+            this.getView().setModel(oModel);
+        },
+
+        getGroupHeader: function (oGroup) {
+            return new GroupHeaderListItem({
+                title: oGroup.key,
+                upperCase: false
+            });
+        }
+    });
 
     return Controller;
 });
